280244d3672cef19834de89aecb432a04eecae6d
vernonthedev <techjaja2@gmail.com>
2024-11-11 17:04:08 -0600

    #Update: Cleaned project and created basic file setup for the new custom purchase module.

---
+++ custom_addons/custom_purchase/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'Extended Purchase Management',
+    'description': 'Module extends the standard purchase model with multi vendor support, bid management, purchase request flow',
+    'author': 'Masamba Vernon',
+    'license':'LGPL-3'
+}
